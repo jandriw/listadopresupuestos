64b47e86f3717bf1086a694f5cefd7e751a38141
jandriw <jan.barcos@gmail.com>
2024-02-27 07:09:11 +0100

    configuración básica supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,22 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+// src/app.d.ts
+
+import { SupabaseClient, Session } from '@supabase/supabase-js'
+
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    interface Locals {
+      supabase: SupabaseClient
+      getSession(): Promise<Session | null>
+    }
+    interface PageData {
+      session: Session | null
+    }
+    // interface Error {}
+    // interface Platform {}
+  }
 }
 
 export {};
